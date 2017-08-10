5a4e76cc34f23ee6011c2dca745d706bb4da450f
Justin Sankarlal <jsankarlal@gmail.com>
2017-08-10 23:05:48 +0530

    g3

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/header-footer.html');
+  res.sendFile(__dirname + '/home.html');
 });
 
 app.use('/', express.static(__dirname + '/'));
 
-app.listen(3000, function () {
-  console.log('app listening on port 3000!');
+app.listen(9000, function () {
+  console.log('app listening on port 9000!');
 });
