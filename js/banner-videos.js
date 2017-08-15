44f03a5e4a5eda7076a314baa227b3a4c1614494
Justin Sankarlal <jsankarlal@gmail.com>
2017-08-15 12:38:47 +0530

    contact us size reduced

--- js/banner-videos.js
+++ js/banner-videos.js
@@ -24,28 +24,6 @@
             });
 //jscs:disable
             scaleBannerVideoSize(element);
-            /* affix the navbar after scroll below header 
-            $('#nav').affix({
-                  offset: {
-                    top: $('.homepage-hero-module').height()
-                  }
-            });	*/
-
-            /* highlight the top nav as scrolling occurs */
-           /* $('body').scrollspy({ target: '#nav' })*/
-
-            /* smooth scrolling for scroll to top 
-            $('.scroll-top').click(function(){
-              $('body,html').animate({scrollTop:0},1000);
-            })*/
-
-            /* smooth scrolling for nav sections */
-           /* $('#nav .navbar-nav li>a').click(function(){
-              var link = $(this).attr('href');
-              var posi = $(link).offset().top - $('#global-navigation').height() - $('#nav').height();
-              event.preventDefault();
-              $('body,html').animate({scrollTop:top}, 1500);
-            });*/
 //jscs:enable
         }
 
